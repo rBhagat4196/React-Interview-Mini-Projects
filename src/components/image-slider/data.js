0f46a55632ee577319b5a9641059bb624ae6ba68
rBhagat4196 <rahulbhagat13slr@gmail.com>
2024-02-10 21:38:23 +0530

    image slider

---
+++ src/components/image-slider/data.js
@@ -0,0 +1,7 @@
+export const images = [
+    "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
+    "https://picjumbo.com/wp-content/uploads/man-looking-at-heart-shaped-explosion-free-photo.jpg",
+    "https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630",
+    "https://images.pexels.com/photos/39853/woman-girl-freedom-happy-39853.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_640.jpg",
+  ];
---
+++ src/components/image-slider/index.jsx
@@ -0,0 +1,75 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import { images } from "./data";
+
+const Slider = () => {
+  
+  const [index, setIndex] = useState(0);
+
+  const handleChange = (by) => {
+    let len = images.length;
+    const newIndex = (index + by + len) % len;
+    setIndex(newIndex);
+    // console.log(newIndex);
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      handleChange(1);
+    }, 4000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [index]);
+
+  const handleClick = (e)=>{
+    const newIndex = images.findIndex( image => image === e.target.src);
+    setIndex(newIndex)
+  }
+  return <>
+    <div className="mx-auto flex gap-2 h-[400px]">
+      <div className="my-auto">
+        <FaAngleLeft
+          className="h-[40px] w-[40px] hover:bg-[#666666] hover:cursor-pointer"
+          onClick={() => handleChange(-1)}
+        />
+      </div>
+      <div className="flex-grow">
+        {images.map((img, idx) => (
+          <img
+            key={img}
+            src={img}
+            alt="img"
+            className={`rounded-xl h-[400px] w-[400px] object-cover ${
+              index === idx ? "flex" : "hidden"
+            }`}
+          />
+        ))}
+      </div>
+      <div className="my-auto">
+        <FaAngleRight
+          className="h-[40px] w-[40px] hover:bg-[#666666] hover:cursor-pointer"
+          onClick={() => handleChange(1)}
+        />
+      </div>
+    </div>
+    <div className="mx-auto flex items-center gap-2 item">
+      {
+        images.map((img,idx)=>(
+  
+          <img
+            key={idx} onClick={handleClick}
+            src={img}
+            alt={img}
+            className={`h-[40px] w-[40px] object-cover rounded-md ${idx === index ? "border-2 border-blue-400 h-[60px] w-[60px]" : "opacity-70"}`}
+            />
+    
+        ))
+      }
+    </div>
+  </>;
+};
+
+export default Slider;
